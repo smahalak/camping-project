a5b15fc18219a1996b038103479e9786a388d7de
Steven <Mahalak.steve@outlook.com>
2021-03-15 15:51:13 -0400

    showing facility names

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CampingServiceService } from '../camping-service.service';
 
 @Component({
@@ -11,18 +12,45 @@ export class HomeComponent implements OnInit {
   // Added to be able to listen to state from child 'search-criteria'
   stateData!: string;
   campData: any;
-  constructor(private campService: CampingServiceService) {}
+  states: any;
+  state: string | null = null;
+  stateInfo: any;
+
+  constructor(private campService: CampingServiceService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    // this.getCamps();
+    this.route.queryParamMap.subscribe((response) => {
+      console.log(response)
+      let state: string | null = response.get('state');
+      if (state) {
+        this.campService.getCampingState(state).subscribe((data) => {
+          console.log(data);
+          this.stateInfo = data;
+        })
+      }
+    })
   }
-  // getCamps = (): void => {
-  //   this.campService.getCampingSites().subscribe((response: any) => {
-  //     // console.log(response);
-  //     this.campData = response;
-  //   });
-  // };
+
+  getAndSetState = (state: any): void => {
+    this.campService.getCampingState(state).subscribe((response: any) => {
+      this.states = response;
+      console.log(response);
+    })
+  }
+
   onStateSearch = (state: string) => {
-    this.stateData = state;
+    this.router.navigate(["/home"],
+      {
+        queryParams: {
+          state: state
+        }
+      }
+    )
+    // this.campService.getCampingState(state).subscribe((response) => {
+    //   console.log(response);
+    //   this.stateInfo = response;
+    // })
   };
+
+
 }
