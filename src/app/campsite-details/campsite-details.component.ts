dae85e5d58cf49b130ae2e4d886f3fddc0ee35c7
Steven <Mahalak.steve@outlook.com>
2021-03-23 10:17:50 -0400

    add supplies button and details styling

--- src/app/campsite-details/campsite-details.component.ts
+++ src/app/campsite-details/campsite-details.component.ts
@@ -1,19 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CampingServiceService } from '../camping-service.service';
-
 @Component({
   selector: 'app-campsite-details',
   templateUrl: './campsite-details.component.html',
   styleUrls: ['./campsite-details.component.css'],
 })
 export class CampsiteDetailsComponent implements OnInit {
+  @Output() watchListEvent = new EventEmitter<any>();
+  @Input() campRef: any;
+  @Input() watchListRef: any = [];
   campsite: any;
   constructor(
     private route: ActivatedRoute,
     private campService: CampingServiceService
-  ) {}
-
+  ) { }
   ngOnInit(): void {
     this.route.paramMap.subscribe((response) => {
       let id: string = response.get('id');
@@ -24,11 +25,19 @@ export class CampsiteDetailsComponent implements OnInit {
           console.log(data);
           this.campsite = data[0];
           // console.log(data[0].ATTRIBUTES);
-          // this.atrributes = data[0].ATTRIBUTES;
+          // this.attributes = data[0].ATTRIBUTES;
           // return (this.campsiteAttributeArray = data);
           // console.log(this.campsiteAttributeArray);
         });
       }
     });
   }
+  emitWatchListEvent = (campsiteName: any): void => {
+    this.watchListEvent.emit(campsiteName);
+  };
+  checkWatchList = (campsiteName: any): boolean => {
+    return this.watchListRef.some((item) => {
+      return item.CampsiteID === campsiteName.CampsiteID;
+    });
+  };
 }
--- src/app/supplies/supplies.component.ts
+++ src/app/supplies/supplies.component.ts
@@ -10,530 +10,630 @@ import { Supply } from '../interfaces/supply';
 export class SuppliesComponent implements OnInit {
   onPacking: boolean = false;
 
+  onDefault: boolean = true;
   onCamping: boolean = false;
   onKitchen: boolean = false;
   onBBQ: boolean = false;
   onFirepit: boolean = false;
   onElectric: boolean = false;
-  onDefault: boolean = true;
 
-  constructor() {}
+
+
+  constructor() { }
 
   supplies: Supply[] = [
     {
       name: 'Tent',
       type: 'Default',
       needed: true,
+
     },
     {
       name: 'Tarp',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Sleeping bag',
       type: 'Default',
       needed: true,
+
     },
     {
       name: 'Pillow',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Axe',
       type: 'Camping',
       needed: true,
-    },
-    {
-      name: '',
-      type: 'Camping',
-      needed: true,
+
     },
     {
       name: 'Knife',
       type: 'Default',
       needed: true,
+
     },
     {
       name: 'Fishing Pole',
       type: 'Default',
       needed: true,
+
     },
     {
       name: 'Water jugs',
       type: 'Default',
       needed: true,
+
     },
+
     {
       name: 'Plates/Bowls',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Cutting board',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Coffee maker',
       type: 'Electric',
       needed: true,
+
     },
     {
       name: 'Air pump',
       type: 'Electric',
       needed: true,
+
     },
     {
       name: 'Portable stove',
       type: 'Electric',
       needed: true,
+
     },
     {
       name: 'Griddle',
       type: 'Electric',
       needed: true,
+
     },
     {
       name: 'Air mattress',
       type: 'Electric',
       needed: true,
+
     },
     {
       name: 'Air pump',
       type: 'Electric',
       needed: true,
+
     },
     {
       name: 'Phone charger',
       type: 'Electric',
       needed: true,
+
     },
     {
       name: 'Firewood',
       type: 'Firepit',
       needed: true,
+
     },
     {
       name: 'Roasting sticks',
       type: 'Firepit',
       needed: true,
+
     },
     {
       name: 'Fire starters',
       type: 'Firepit',
       needed: true,
+
     },
     {
       name: 'Pudgy pie iron',
       type: 'Firepit',
       needed: true,
+
     },
     {
       name: 'Flint striker',
       type: 'Firepit',
       needed: true,
+
     },
     {
       name: 'Fire Pit Grill',
       type: 'Firepit',
       needed: true,
+
     },
     {
       name: 'Fire pit pot (& pot lifter)',
       type: 'Firepit',
       needed: true,
+
     },
     {
       name: 'Dutch oven',
       type: 'Firepit',
       needed: true,
+
     },
     {
       name: 'Charcoal',
       type: 'BBQ',
       needed: true,
+
     },
     {
       name: 'Charcoal lighter fluid',
       type: 'BBQ',
       needed: true,
+
     },
     {
       name: 'Sleeping pads',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Water',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Folding chairs',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Butane lighter',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'First aid kit',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Lantern fuel',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Batteries',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Headlamp',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Bug spray',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Matchbox',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Scissors',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Toothbrush',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Toothpaste',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Hat/cap',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Swimsuit',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Binoculars',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Candles',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Pen and paper',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Whistle',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Sunblock',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Flashlight',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Rope',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Cups/mugs',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Cups/mugs',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Outdoor tableware',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Utensils',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Dish brush/scraper',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Hand sanitizer',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Towel',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Mosquito incense repellent',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Mosquito net',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Portable propane stove',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Portable charcoal grill',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Folding chairs',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Camp table',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Table cloth',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Hammock',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Multi-tool',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Duct tape',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Mosquito net',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Mallet or hammer',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Tent stakes',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Broom/dustpan',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Trash/recycling bags',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Bottle opener',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Can opener',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Cool and dry food storage containers',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Foil',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Bags',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Navigation tools',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Wildlife and field guides',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Dry bag/stuff sack',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Star Chart/night-sky identifier',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Moisture-wicking clothing',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Boots/shoes/sandals',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Sleepwear',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Jacket',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Rainwear',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Swim suit',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Toilet paper',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Menstrual products',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Prescription medications',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Baby wipes',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Brush/comb',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Mirror',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Sunshade, tarp or screen house',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Deodorant',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Razer/shaving cream',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Shampoo',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Cooking oil/Pam spray',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Mixing bowls',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Measuring cups',
       type: 'Kitchen',
       needed: true,
+
     },
     {
       name: 'Water filter/purifier',
       type: 'Camping',
       needed: true,
+
     },
     {
       name: 'Charcoal lighter fluid',
       type: 'BBQ',
       needed: true,
+
     },
     {
       name: 'Charcoal',
       type: 'BBQ',
       needed: true,
+
     },
   ];
 
@@ -567,6 +667,7 @@ export class SuppliesComponent implements OnInit {
       name: form.form.value.name,
       type: form.form.value.type,
       needed: true,
+
     };
 
     if (!this.find(form.form.value.name)) {
@@ -579,18 +680,16 @@ export class SuppliesComponent implements OnInit {
     item.needed = !item.needed;
   };
 
-  showDiv = (onPacking: boolean): void => {
-    this.onPacking = !this.onPacking;
-  };
-  showPackingList = (): Supply[] => {
-    return this.supplies.filter((item) => {
-      return item.needed === true;
-    });
-  };
+  // showDiv = (onPacking: boolean): void => {
+  //   this.onPacking = !this.onPacking;
+  // };
+  // showPackingList = (): Supply[] => {
+  //   return this.supplies.filter((item) => {
+  //     return item.needed === true;
+  //   });
+  // };
 
   showFilteredCampingCategory = (type: string): void => {
-    // return this.supplies.filter((item) => {
-    // item.type === type;
     if (type === 'Camping') {
       this.onCamping = !this.onCamping;
     } else if (type === 'Kitchen') {
@@ -604,7 +703,19 @@ export class SuppliesComponent implements OnInit {
     } else if (type === 'Default') {
       this.onDefault = !this.onDefault;
     }
-  };
+
+    if (type === 'All') {
+      this.onCamping = true;
+      this.onKitchen = true;
+      this.onBBQ = true;
+      this.onFirepit = true;
+      this.onElectric = true;
+      this.onDefault = true;
+    }
+  }
+
+
+
   showFilteredList = (type: string): Supply[] => {
     return this.supplies.filter((item) => {
       return item.type === type;
